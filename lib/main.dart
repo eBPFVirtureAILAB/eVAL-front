fddb751a70360cfcecd239c2740cefacd3a168c7
O3Olivia <cherishtmwu@gmail.com>
2024-05-31 22:49:44 +0900

    modify main.dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Evil',
       theme: ThemeData(
         primarySwatch: Colors.purple,
         brightness: Brightness.dark,
+        textTheme: GoogleFonts.mulishTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.black),
       ),
       home: MyHomePage(),
     );
